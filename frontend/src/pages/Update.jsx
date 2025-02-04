567bec680be521292889c41282f7ae9c35ce2225
Junjia Wang <jwang268@dons.usfca.edu>
2025-02-04 18:28:59 -0500

    add dockerfile

--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -1,52 +1,69 @@
-import React, { useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
 //rafce
 const Update = () => {
+  const [book, setBook] = useState({
+    title: "",
+    description: "",
+    price: null,
+    cover: "",
+  });
 
+  const navigate = useNavigate();
 
-    const [book, setBook] = useState({
-        title: "",
-        description: "",
-        price: null,
-        cover: ""
-    });
+  const location = useLocation();
 
-    const navigate = useNavigate()
+  const bookID = location.pathname.split("/")[2];
 
-    const location = useLocation()
+  console.log(location.pathname.split("/")[2]);
 
-    const bookID = location.pathname.split("/")[2]
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
 
-    console.log(location.pathname.split("/")[2])
-
-    const handleChange = (e) => {
-        setBook(prev=>({...prev, [e.target.name]: e.target.value }));
-    };
-
-   const handleClick = async e =>{
-    e.preventDefault()
-    try{
-        await axios.put("http://localhost:8800/books/"+ bookID, book)
-        navigate("/")
-    }catch(err){
-        console.log(err)
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put("http://localhost:3001/books/" + bookID, book);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
     }
-   }
-console.log(book)
+  };
+  console.log(book);
   return (
-    <div className='form'>
-   <h1>Update Book</h1>
-   <input type="text" placeholder="title" onChange={handleChange} name="title"/>
-   <input type="text" placeholder="description" onChange={handleChange} name="description"/>
-   <input type="number" placeholder="price" onChange={handleChange} name="price"/>
-   <input type="text" placeholder="cover" onChange={handleChange} name="cover"/>
-   <button onClick={handleClick} className='updateButton'>
-   Update
-   </button>
+    <div className="form">
+      <h1>Update Book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="description"
+        onChange={handleChange}
+        name="description"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+      <button onClick={handleClick} className="updateButton">
+        Update
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Update
-
+export default Update;
